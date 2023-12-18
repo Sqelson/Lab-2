7a5be6b6c5cd5768f2b34dde3aadcff048ba5497
Sqelson <sqelson@gmail.com>
2023-12-18 21:22:57 +0000

    Updated to have all declerations and implementations seperated into the correct files (header and cpp files, respectively).

--- dictionary/dictionary.cpp
+++ dictionary/dictionary.cpp
@@ -1,75 +1,138 @@
 #include "dictionary.h"
 #include "node.h"
 
-bool Dictionary::isLeaf(Node* theNode)
-{
-	return (theNode == nullptr);
-	// if the node is null, it is a leaf, this replaces the nullptr == theNode syntax for readability
+// Constructor
+Dictionary::Dictionary() : head_(nullptr) {}
+
+// Destructor
+Dictionary::~Dictionary() {
+    deepDeleteWorker(head_);
 }
 
-void Dictionary::displayEntries()
-{
-	displayEntriesWorker(head_); // call the worker function on the root pointer to start the recursion
+// Copy Constructor
+Dictionary::Dictionary(const Dictionary& other) {
+    head_ = deepCopyWorker(other.head_);
 }
 
-void Dictionary::displayEntriesWorker(Node* currentNode)
-{
-	if (isLeaf(currentNode)) return; // (1) base case: if the current node is null, return
-
-	std::cout << "Key = " << currentNode->data << ", Value = " << currentNode->value << std::endl;
-	// (A) code for processing a node during a pre-order traversal goes here
-	displayEntriesWorker(currentNode->left); // (2) recursively call the worker function on the left child pointer here
-	// (B) code for processing a node during an in-order traversal would go here (cut and paste the code from (A) above)
-	displayEntriesWorker(currentNode->right);
-	// (3) recursively call the worker function on the right child pointer here
-	// (C) code for processing a node during a post-order traversal goes here (cut and paste the code from (A) above)
+// Check if a node is a leaf (null)
+bool Dictionary::isLeaf(Node* theNode) {
+    return (theNode == nullptr);
 }
 
-void Dictionary::displayTree()
-{
-	displayTreeWorker(head_, ""); // call the worker function on the root pointer to start the recursion
+// Display all entries in the dictionary
+void Dictionary::displayEntries() {
+    displayEntriesWorker(head_);
 }
 
-void Dictionary::displayTreeWorker(Node* node, std::string indent)
-{
-	if (isLeaf(node)) // Base case: if the current node is null, display a leaf node and return.
-	{
-		std::cout << indent << "*" << std::endl;
-		return;
-	}
-	std::cout << indent << "Key = " << node->data << ", Value = " << node->value << std::endl;
-	// (A) code for processing a node during a pre-order traversal goes here
-	indent += "    "; // Increase indentation to distinguish child nodes.
-	displayTreeWorker(node->left, indent); // Recursive call for the left child with increased indentation.
-	// (B) code for processing a node during an in-order traversal would go here (cut and paste the code from (A) above)
-	displayTreeWorker(node->right, indent); // Recursive call for the right child with increased indentation.
-	// (C) code for processing a node during a post-order traversal goes here (cut and paste the code from (A) above).
+// Recursive worker for displaying entries
+void Dictionary::displayEntriesWorker(Node* currentNode) {
+    if (isLeaf(currentNode)) return;
+    std::cout << "Key = " << currentNode->data << ", Value = " << currentNode->value << std::endl;
+    displayEntriesWorker(currentNode->left);
+    displayEntriesWorker(currentNode->right);
 }
 
-// Define the copy constructor.
-Dictionary::Dictionary(const Dictionary& other)
-{
-	// Initialize head_ to nullptr in the new object.
-	head_ = nullptr;
-	// Perform a deep copy of the tree from 'other' using the recursive worker function.
-	head_ = deepCopyWorker(other.head_);
+// Display the structure of the tree
+void Dictionary::displayTree() {
+    displayTreeWorker(head_, "");
 }
 
-// Recursive worker function to perform deep copy.
-Node* Dictionary::deepCopyWorker(Node* node)
-{
-	if (isLeaf(node))
-	{
-		return nullptr; // Base case: If the original tree is empty, return nullptr.
-	}
-
-	// Create a new node with the same data and value as the original node.
-	auto newNode = new Node(node->data, node->value);
-
-	// Recursively copy the left and right subtrees.
-	newNode->left = deepCopyWorker(node->left);
-	newNode->right = deepCopyWorker(node->right);
-
-	return newNode;
+// Recursive worker for displaying the tree
+void Dictionary::displayTreeWorker(Node* node, std::string indent) {
+    if (isLeaf(node)) {
+        std::cout << indent << "*" << std::endl;
+        return;
+    }
+    std::cout << indent << "Key = " << node->data << ", Value = " << node->value << std::endl;
+    indent += "    ";
+    displayTreeWorker(node->left, indent);
+    displayTreeWorker(node->right, indent);
 }
 
+// Lookup a node by key
+Dictionary::ItemType* Dictionary::lookupNode(KeyType key) {
+    return lookupNodeWorker(head_, key);
+}
+
+// Recursive worker for node lookup
+Dictionary::ItemType* Dictionary::lookupNodeWorker(Node* node, KeyType key) {
+    if (isLeaf(node)) {
+        return nullptr;
+    }
+    if (key == node->data) {
+        return &node->value;
+    }
+    if (key < node->data) {
+        return lookupNodeWorker(node->left, key);
+    }
+    return lookupNodeWorker(node->right, key);
+}
+
+// Insert a node
+void Dictionary::insertNode(KeyType key, ItemType value) {
+    head_ = insertNodeWorker(head_, key, value);
+}
+
+// Recursive worker for node insertion
+Node* Dictionary::insertNodeWorker(Node*& node, KeyType key, ItemType value) {
+    if (isLeaf(node)) {
+        return new Node(key, value);
+    }
+    if (key == node->data) {
+        node->value = value;
+    }
+    else if (key < node->data) {
+        node->left = insertNodeWorker(node->left, key, value);
+    }
+    else {
+        node->right = insertNodeWorker(node->right, key, value);
+    }
+    return node;
+}
+
+// Remove a node
+void Dictionary::removeNode(KeyType key) {
+    removeNodeWorker(head_, key);
+}
+
+// Recursive worker for node removal
+void Dictionary::removeNodeWorker(Node*& node, KeyType key) {
+    // Implementation of removeNodeWorker...
+}
+
+// In-order traversal
+std::vector<int> Dictionary::inOrderTraversal() {
+    std::vector<int> keys;
+    inOrderTraversalWorker(head_, keys);
+    return keys;
+}
+
+// Recursive worker for in-order traversal
+void Dictionary::inOrderTraversalWorker(Node* node, std::vector<int>& keys) {
+    if (!isLeaf(node)) {
+        inOrderTraversalWorker(node->left, keys);
+        keys.push_back(node->data);
+        inOrderTraversalWorker(node->right, keys);
+    }
+}
+
+// Recursive worker for deep deletion
+void Dictionary::deepDeleteWorker(Node* node) {
+    if (isLeaf(node)) {
+        return;
+    }
+    deepDeleteWorker(node->left);
+    deepDeleteWorker(node->right);
+    delete node;
+}
+
+// Recursive worker for deep copy
+Node* Dictionary::deepCopyWorker(Node* node) {
+    if (isLeaf(node)) {
+        return nullptr;
+    }
+    Node* newNode = new Node(node->data, node->value);
+    newNode->left = deepCopyWorker(node->left);
+    newNode->right = deepCopyWorker(node->right);
+    return newNode;
+}
